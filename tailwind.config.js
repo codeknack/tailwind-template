816ece5f8b50dff9aa351ccffca296b28c37ed81
codeknack <biz.mailbox@abv.bg>
2019-08-19 01:19:30 +0300

    deploy

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    extend: {
+      borderRadius: {
+        xl: '1rem',
+        '2xl': '2rem',
+        '3xl': '3rem'
+      }
+    }
+  },
+  variants: {
+    margin: ['responsive', 'hover']
+  },
+  plugins: []
+}
